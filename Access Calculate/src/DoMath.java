d5f482bf48ff65eceebcefbe811e2ced068f932b
Mitra Pourmehraban <18pourmehrabanm50@millshighsch.org>
2017-09-14 21:39:47 -0700

    finished part 2 from home computer.

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -11,6 +11,16 @@ public class DoMath {
 		System.out.println(Calculate.average(4,5,6));
 		System.out.println(Calculate.toDegrees(3.14159));
 		System.out.println(Calculate.toRadians(180));
+		System.out.println(Calculate.discriminant(2.1, 1.6, 3.1));
+		System.out.println(Calculate.toImproperFrac(3, 1, 2));
+		System.out.println(Calculate.toMixedNum(7, 2));
+		System.out.println(Calculate.foil(2, 3, 6, -7, "n"));
+		System.out.println(Calculate.isDivisibleBy(6, 3));
+		System.out.println(Calculate.absValue(-.3));
+		System.out.println(Calculate.max(2.3, -2.4));
+		System.out.println(Calculate.max(2.3, -2.4, 5.5));
+		System.out.println(Calculate.min(2, 0));
+		System.out.println(Calculate.round2(2.555));
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,135 +4,168 @@
  * mathematical methods.
  */
 public class Calculate {
-/* This method takes an integer and returns its square.
- * 
- */
+	/*
+	 * This method takes an integer and returns its square.
+	 * 
+	 */
 	public static int square(int operand) {
 		return operand * operand;
-	}	
-/* This method takes an integer and returns its cube.
- * 
- */
+	}
+
+	/*
+	 * This method takes an integer and returns its cube.
+	 * 
+	 */
 	public static int cube(int operand) {
 		return operand * operand * operand;
 	}
-/* This method takes 2 doubles and returns the average of them.
- * 
- */
+
+	/*
+	 * This method takes 2 doubles and returns the average of them.
+	 * 
+	 */
 	public static double average(double operand1, double operand2) {
-		double average = (operand1 + operand2)/2;
+		double average = (operand1 + operand2) / 2;
 		return average;
 	}
-/* This method takes 3 doubles and returns the average of them.
- * 
- */
+
+	/*
+	 * This method takes 3 doubles and returns the average of them.
+	 * 
+	 */
 	public static double average(double operand1, double operand2, double operand3) {
-		double average=(operand1 + operand2 + operand3)/3;
+		double average = (operand1 + operand2 + operand3) / 3;
 		return average;
 	}
-/* This method converts an angle measure in radians to degrees.
- * 
- */
+
+	/*
+	 * This method converts an angle measure in radians to degrees.
+	 * 
+	 */
 	public static double toDegrees(double operand) {
-			double toDegrees=(operand * (180/3.14159));
-			return toDegrees;
+		double toDegrees = (operand * (180 / 3.14159));
+		return toDegrees;
 	}
-/* This method converts an angle measure in radians to degrees.
- * 
- */
+
+	/*
+	 * This method converts an angle measure in radians to degrees.
+	 * 
+	 */
 	public static double toRadians(double operand) {
-			double toRadians=(operand * (3.14159/180));
-			return toRadians;
+		double toRadians = (operand * (3.14159 / 180));
+		return toRadians;
 	}
-/* This method determines the discrimant from the three coefficients in standard form.
- *
- */
+
+	/*
+	 * This method determines the discriminant from the three coefficients in
+	 * standard form.
+	 *
+	 */
 	public static double discriminant(double a, double b, double c) {
-			double discriminant=(b * b) - (4 * a * c);
-			return discriminant;
+		double discriminant = (b * b) - (4 * a * c);
+		return discriminant;
 	}
-/* This method takes a mixed number and converts it to an improper fraction.
- *
- */
+
+	/*
+	 * This method takes a mixed number and converts it to an improper fraction.
+	 *
+	 */
 	public static String toImproperFrac(int coefficient, int numerator, int denominator) {
-			double numeratorFrac = (coefficient * denominator) + numerator;
-			return numeratorFrac + "/" + denominator;
+		int numeratorFrac = (coefficient * denominator) + numerator;
+		return numeratorFrac + "/" + denominator;
 	}
-/* This method takes an improper fraction and converts it into a mixed number.
- *
- */
+
+	/*
+	 * This method takes an improper fraction and converts it into a mixed number.
+	 *
+	 */
 	public static String toMixedNum(int numerator, int denominator) {
-			return numerator/denominator + "_" + numerator % denominator + "/" + denominator;
+		return numerator / denominator + "_" + numerator % denominator + "/" + denominator;
 	}
-/* This method takes four intergers and a string to convert binomial
- * multiplication into the form of a quadratic equation.
- */
+
+	/*
+	 * This method takes four integers and a string to convert binomial
+	 * multiplication into the form of a quadratic equation.
+	 */
 	public static String foil(int a, int b, int c, int d, String letter) {
-			return (a * b + letter + "^2") + "+" + ((a * d) + (b * c) + letter) + "+" + (b * d);
+		return (a * c + letter + "^2") + "+" + ((a * d) + (b * c) + letter) + "+" + (b * d);
 	}
-/* This method takes two integers and determines if one integer if divisible by the other.
- *
- */
+
+	/*
+	 * This method takes two integers and determines if one integer if divisible by
+	 * the other.
+	 *
+	 */
 	public static boolean isDivisibleBy(int dividend, int divisor) {
-			if(dividend % divisor == 0) {
-				return true; }
-			}else {
-				return false;
-}
-	}
-/* This method takes a double and returns the absolute value of that double.
- *
- */
-	public static double absValue(double number) {
-		if(number < 0) {
-			return number * -1;
-}else {
-return number;			
+		if (dividend % divisor == 0) {
+			return true;
+		} else {
+			return false;
 		}
 	}
-/* This method takes a two numbers and determines the max of the values.
- *
- */
+
+	/*
+	 * This method takes a double and returns the absolute value of that double.
+	 *
+	 */
+	public static double absValue(double number) {
+		if (number < 0) {
+			return number * -1;
+		} else {
+			return number;
+		}
+	}
+
+	/*
+	 * This method takes a two numbers and determines the max of the values.
+	 *
+	 */
 	public static double max(double num1, double num2) {
-		if(num1 > num2) {
+		if (num1 > num2) {
 			return num1;
-		}else {
+		} else {
 			return num2;
 		}
 	}
-/* This method takes a three numbers and determines the max of the values.
- *
- */
-public static double max(double num1, double num2, double num3) {
-		if(num1 > num2 & num1 > num3) {
+
+	/*
+	 * This method takes a three numbers and determines the max of the values.
+	 *
+	 */
+	public static double max(double num1, double num2, double num3) {
+		if (num1 > num2 & num1 > num3) {
 			return num1;
-		}else if (num2 > num1 & num2 > num3) {
+		} else if (num2 > num1 & num2 > num3) {
 			return num2;
 		} else {
 			return num3;
 		}
 	}
-/* This method takes a two numbers and determines the min of the values.
- *
- */
-public static double min(double num1, double num2) {
-if(num1 <= num2) {
-		Return num1;
-	}else {
-		Return num2;
-	}
-}
-/* This method rounds a number to two decimal places.
- *
- */
-	Public static double round2(double number) {
-	number = number * 1000;
-	if (number % 10 < 5) {
-		number = number - (number % 10);
-		return number / 1000;
-	} else {
-		number = number - (number % 10) + 10;
-		return number / 1000;
+
+	/*
+	 * This method takes a two numbers and determines the min of the values.
+	 *
+	 */
+	public static double min(double num1, double num2) {
+		if (num1 <= num2) {
+			return num1;
+		} else {
+			return num2;
+		}
 	}
 
+	/*
+	 * This method rounds a number to two decimal places.
+	 *
+	 */
+	public static double round2(double number) {
+		number = number * 1000;
+		if (number % 10 < 5) {
+			number = number - (number % 10);
+			return number / 1000;
+		} else {
+			number = number - (number % 10) + 10;
+			return number / 1000;
+		}
+	}
 }
